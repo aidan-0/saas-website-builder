ce73c974309cad434224566c6a8f7e7e7071756b
Aidan <am.mcdonald00@gmail.com>
2024-09-06 16:37:24 +0800

    set up routing and install prisma

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -1,13 +1,18 @@
 import Navigation from "@/components/site/navigation";
 import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<main>
+		<ClerkProvider appearance={{ baseTheme: dark }}>
+			<main>
 			<Navigation />
 			{children}
 		</main>
+		</ClerkProvider>
 	);
 };
 
 export default layout;
+
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,73 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/forum(.*)'])
+const isPublicRoute = createRouteMatcher([
+	"/agency/sign-in(.*)",
+	"/agency/sign-up(.*)",
+	"/site",
+	"/api/uploadthing",
+]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect()
-})
+	// Enforce authentication for non-public routes
+	if (!isPublicRoute(req)) {
+		auth().protect();
+	}
+
+	// Extract URL and search parameters
+	const url = req.nextUrl;
+	const searchParams = url.searchParams.toString();
+	let hostname = req.headers;
+	console.log("hostname", hostname);
+
+	// Construct the full path with search parameters
+	const pathWithSearchParams = `${url.pathname}${
+		searchParams.length > 0 ? `?${searchParams}` : ""
+	}`;
+
+	// Check for subdomains
+	const customSubDomain = hostname
+		.get("host")
+		?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+		.filter(Boolean)[0];
+
+	// Rewrite URL if a subdomain exists
+	if (customSubDomain) {
+		return NextResponse.rewrite(
+			new URL(`/${customSubDomain}${pathWithSearchParams}`, req.url)
+		);
+	}
+
+	// Redirect specific paths
+	if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
+		return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
+	}
+
+	// Rewrite root and site paths
+	if (
+		url.pathname === "/" ||
+		(url.pathname === "/site" &&
+			url.host === process.env.NEXT_PUBLIC_DOMAIN)
+	) {
+		return NextResponse.rewrite(new URL("/site", req.url));
+	}
+
+	// Rewrite paths starting with /agency or /subaccount
+	if (
+		url.pathname.startsWith("/agency") ||
+		url.pathname.startsWith("/subaccount")
+	) {
+		return NextResponse.rewrite(
+			new URL(`${pathWithSearchParams}`, req.url)
+		);
+	}
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+	matcher: [
+		// Skip Next.js internals and all static files, unless found in search params
+		"/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+		// Always run for API routes
+		"/(api|trpc)(.*)",
+	],
+};
